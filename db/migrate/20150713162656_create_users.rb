1bd0435b2f632f8ca3189120e269ec11ff1b599a
Junho Rhee <juno0628@gmail.com>
2015-07-17 17:07:09 -0400

    add schema fix

--- db/migrate/20150713162656_create_users.rb
+++ db/migrate/20150713162656_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string "user_name"
-    	t.string "user_email"
-        t.string "password_digest"
-    	t.string "f_name"
+    	t.string "user_name", null: false 
+    	t.string "user_email", null: false
+        t.string "password_digest" 
+    	t.string "f_name" 
     	t.string "l_name"
-    	t.string "user_profile_image"
+    	t.string "user_profile_image", default: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTI6rjxsSvJ5_HeACCreH9oAkLEHumy6124cUM4bY8t4bYUzTACcjUwcrY"
     	t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -58,12 +58,12 @@ ActiveRecord::Schema.define(version: 20150717012909) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "user_name"
-    t.string   "user_email"
+    t.string   "user_name",                                                                                                                                      null: false
+    t.string   "user_email",                                                                                                                                     null: false
     t.string   "password_digest"
     t.string   "f_name"
     t.string   "l_name"
-    t.string   "user_profile_image"
+    t.string   "user_profile_image", default: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTI6rjxsSvJ5_HeACCreH9oAkLEHumy6124cUM4bY8t4bYUzTACcjUwcrY"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
