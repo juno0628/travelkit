0fd451d64c3d447a8ccc553acde9683c7baff1ea
Junho Rhee <juno0628@gmail.com>
2015-07-17 05:10:04 -0400

    added styling

---
+++ app/assets/images/TravelPlanner_files/travel-plan-edit.self-8cd00c99c5ff0f10edc244b49014c69b4a8c4614bcba45f6e404c494b1bd7800.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+		$('.show-category').on('click', function() {
+		console.log('clicked');
+		if ($(this).attr('id') != "total") {
+			// sending AJAX to retrieve 
+			var categoryId = $(this).attr('id');
+			var planId =  $('.travel-show-page-title').attr('id');
+			var url = '/travel_plans/'+ planId + '/categories/' + categoryId;
+			
+			// append to template after receiving data from server 
+			$.ajax({
+				url: url,
+				type: 'get'
+			}).done(function(data) {
+					console.log(data);
+					console.log('data received from server');
+					$('.travel-plan-container').html(data);
+			})
+		}
+	})
+});
