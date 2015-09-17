f11b93eab8ff91042ca77f24e4b6cbb846b4a9e8
Junho Rhee <juno0628@gmail.com>
2015-09-17 09:16:15 -0400

    aded

--- app/assets/javascripts/permanent-header.js
+++ app/assets/javascripts/permanent-header.js
@@ -1,23 +1,20 @@
 
-//setting permanent header
-$('document').ready(function() { 
-	if ($('.permanent-header').hasClass('top-bar')) {
-			function ChangeHeader() {
-			var el = $('.permanent-header');
-			var scrolled = el.offset().top;
-			var scrollTop = $(window).scrollTop();
-		
-		//add floatingheader class after scrolling
-		//class already exist  
-			if ((scrollTop > 211) && (!$('.permanent-header').hasClass('floatingHeader'))) {
-				$('.permanent-header').addClass('floatingHeader');
-				$('.permanent-header').addClass('floatingHeader');
+//setting permanent header 
+if ($('.permanent-header').hasClass('top-bar')) {
+	function ChangeHeader() {
+	var el = $('.permanent-header');
+	var scrollTop = $(window).scrollTop();
+	
+	//add floatingheader class after scrolling
+	//class already exist  
+		if ((scrollTop > 211) && (!$('.permanent-header').hasClass('floatingHeader'))) {
+			$('.permanent-header').addClass('floatingHeader');
+			$('.permanent-header').addClass('floatingHeader');
 
-			} else if (scrollTop <= 211) {
-				$('.permanent-header').removeClass('floatingHeader');
-			};
+		} else if (scrollTop <= 211) {
+			$('.permanent-header').removeClass('floatingHeader');
 		};
+	};
 	$(window).scroll(ChangeHeader)
 	$(window).trigger('scroll');
-	};
 };
